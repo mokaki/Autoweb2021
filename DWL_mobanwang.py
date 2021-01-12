957840a8a39ef0455e4b0f54e2578047e0a363af
DustGroup <56112369+mokaki@users.noreply.github.com>
2021-01-12 18:51:57 +0800

    Add files via upload

---
+++ DWL_mobanwang.py
@@ -0,0 +1,29 @@
+﻿
+# -*- coding: utf-8 -*-
+#AutoDWL_mobanwang
+#202101121845
+#!/usr/bin/python3
+#!python*
+
+# https://code.tutsplus.com/tutorials/how-to-download-files-in-python--cms-30099
+
+import requests
+import urllib
+
+
+DwlNob = input('請問您想下載的網頁編號?')
+
+
+url = 'http://www.mobanwang.com/mb/showsoftdown.asp?urlid=1&softid=' + DwlNob
+rar = DwlNob + '.rar'
+
+
+r = requests.get(url)
+with open(rar, "wb") as code:
+	code.write(r.content)
+
+urllib.urlretrieve(url, rar)
+
+
+
+os.system("pause")
